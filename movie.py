832b74e54313410013473f2a8b09d42eaa098c85
ahmedsaalah <a7mad.sala7@live.com>
2017-02-25 22:14:52 +0200

    after review

--- movie.py
+++ movie.py
@@ -1,9 +1,12 @@
 import webbrowser
-class Movie():
+
+
+class Movie ():
     '''
     This class consists of main attributes of the movies
     '''
-    def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube):
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
         """
                constructor
 
@@ -14,13 +17,7 @@ class Movie():
                     trailer_youtube(str): trailer url on youtube
 
         """
-        self.title=movie_title
-        self.storyline=movie_storyline
-        self.poster_image_url=poster_image
-        self.trailer_youtube_url=trailer_youtube
-
-
-
-
-
-
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
--- redme.md
+++ redme.md
@@ -7,6 +7,13 @@ This project is movie app web page allowing visitors to browse their movies and 
 This app  help you in easy way to see trailers and show reviews
 ## Installation
 
-you need to install python to run that project
+you need to install python to run that project then open project folder
+and run the cmd write python entertainment.py
+
+## note
+ If the project didn't open browser the reason  may be that you make that deafult of html
+ file open with the opened application
+
+
 
 
